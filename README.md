9f19f41842b555d0d31479c658a97bc8a9e6fd16
Aaron Brier <aj567br@gmail.com>
2021-01-01 13:52:58 -0800

    now performs sentiment analysis on comments from hot posts

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WSB-redditbot
 A Reddit bot that monitors /r/WallStreetBets
+
+Dependencies:
+* pip3 install praw
+* pip3 install -U textblob
+* python3 -m textblob.download_corpora
+* CLIENT_ID and CLIENT_SECRET obtained from https://www.reddit.com/prefs/apps
--- script.py
+++ script.py
@@ -1,1 +1,23 @@
-import creds
+import re
+from creds import *
+import praw
+from textblob import TextBlob
+
+def clean(comment): 
+	#clean comments by removing links, special characters  
+	return ' '.join(re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", comment).split())
+
+def analyze(all_comments):
+	total = 0
+	for comment in all_comments:
+		if not isinstance(comment, praw.models.MoreComments):
+			total += TextBlob(clean(comment.body)).sentiment.polarity
+	return total
+
+total = 0
+reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT)
+for submission in reddit.subreddit("wallstreetbets").hot(limit=10):
+	print(submission.title)
+	all_comments = submission.comments.list()
+	total += analyze(all_comments)
+	print(total)
